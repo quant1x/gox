af04a5eb60c4929d0bb0ad5e8c327608cdcfdd41
王锋 <wangfengxy@sina.cn>
2019-04-14 02:12:47 +0800

    fix go test

--- fastjson/parser_timing_test.go
+++ fastjson/parser_timing_test.go
@@ -172,14 +172,14 @@ func BenchmarkParse(b *testing.B) {
 
 var (
 	// small, medium and large fixtures are from https://github.com/buger/jsonparser/blob/f04e003e4115787c6272636780bc206e5ffad6c4/benchmark/benchmark.go
-	smallFixture  = getFromFile("testdata/small.fastjson")
-	mediumFixture = getFromFile("testdata/medium.fastjson")
-	largeFixture  = getFromFile("testdata/large.fastjson")
+	smallFixture  = getFromFile("testdata/small.json")
+	mediumFixture = getFromFile("testdata/medium.json")
+	largeFixture  = getFromFile("testdata/large.json")
 
 	// canada, citm and twitter fixtures are from https://github.com/serde-rs/json-benchmark/tree/0db02e043b3ae87dc5065e7acb8654c1f7670c43/data
-	canadaFixture  = getFromFile("testdata/canada.fastjson")
-	citmFixture    = getFromFile("testdata/citm_catalog.fastjson")
-	twitterFixture = getFromFile("testdata/twitter.fastjson")
+	canadaFixture  = getFromFile("testdata/canada.json")
+	citmFixture    = getFromFile("testdata/citm_catalog.json")
+	twitterFixture = getFromFile("testdata/twitter.json")
 )
 
 func getFromFile(filename string) string {
