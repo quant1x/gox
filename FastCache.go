94526b940850d1b3b75dd40cffaaf5dae5c5fbaf
mymmsc <wangfeng@yeah.net>
2019-04-16 21:34:45 +0800

    fix

---
+++ FastCache.go
@@ -0,0 +1,43 @@
+package gox
+
+import (
+	"os"
+	"path/filepath"
+	"syscall"
+)
+
+type FastCache struct {
+	filename string
+	f        *os.File
+	size     int64
+	Data     []byte
+}
+
+func OpenCache(name string, size int64) (*FastCache, error) {
+	dir := filepath.Dir(name)
+	os.MkdirAll(dir, 0755)
+	filename := name
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	if nil != err {
+		return nil, err
+	}
+	f.Truncate(size)
+	data, err := syscall.Mmap(int(f.Fd()), 0, int(size), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
+	if nil != err {
+		return nil, err
+	}
+
+	return &FastCache{
+		filename: filename,
+		f:        f,
+		size:     size,
+		Data:     data,
+	}, nil
+}
+
+func (fc *FastCache) Close() {
+	if fc != nil && fc.f != nil {
+		_ =fc.f.Close()
+		_ =syscall.Munmap(fc.Data)
+	}
+}
