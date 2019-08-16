c9bcc4bb9700e5710889a5a71a4df7d9eae358cd
mymmsc <wangfengxy@sina.cn>
2019-08-17 06:16:39 +0800

    fix init

--- mdc/mdc.go
+++ mdc/mdc.go
@@ -2,11 +2,21 @@ package mdc
 
 import (
 	"github.com/mymmsc/gox/gls"
+	uuid "github.com/satori/go.uuid"
 )
 
 const (
 	APP_TRACEID = "app-traceid"
 )
+
+func init() {
+	RefreshTraceId()
+}
+
+func RefreshTraceId()  {
+	u1 := uuid.NewV4()
+	Set(APP_TRACEID, u1.String())
+}
 
 func Set(key string, value interface{}) {
 	gls.Set(key, value)
