565736cf78fc0e95a57b3f69247b4aaf256937f0
wangfeng <wangfengxy@sina.cn>
2020-08-01 16:15:01 +0800

    add encoding

---
+++ encoding/mahoniconv/mahoniconv.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"github.com/mymmsc/gox/encoding"
+	"io"
+	"log"
+	"os"
+)
+
+// An iconv workalike using mahonia.
+var from = flag.String("f", "utf-8", "source character set")
+var to = flag.String("t", "utf-8", "destination character set")
+
+func main() {
+	flag.Parse()
+
+	var r io.Reader = os.Stdin
+	var w io.Writer = os.Stdout
+
+	if *from != "utf-8" {
+		decode := encoding.NewDecoder(*from)
+		if decode == nil {
+			log.Fatalf("Could not create decoder for %s", *from)
+		}
+		r = decode.NewReader(r)
+	}
+
+	if *to != "utf-8" {
+		encode := encoding.NewEncoder(*to)
+		if encode == nil {
+			log.Fatalf("Could not create decoder for %s", *to)
+		}
+		w = encode.NewWriter(w)
+	}
+
+	io.Copy(w, r)
+}
