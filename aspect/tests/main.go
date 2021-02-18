d7ecf2d9926d53f4546c65129475761f5fc5ce51
wangfeng <wangfengxy@sina.cn>
2021-02-18 18:37:18 +0800

    add AOP

---
+++ aspect/tests/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/go-xorm/xorm"
+	"github.com/mymmsc/gox/aspect/annotation/transaction"
+	"github.com/mymmsc/gox/aspect/example"
+)
+
+// go build -gcflags=-l main.go
+// main
+func main() {
+	scanPath := `/Users/wangfeng/projects/mymmsc/gox/aspect/example`
+	transaction.NewTransactionManager(transaction.TransactionConfig{ScanPath: scanPath}).RegisterDao(new(example.ExampleDao))
+
+	dao := new(example.ExampleDao)
+	dao.Select()
+	dao.Update(new(xorm.Session), "") // auto commit
+	dao.Delete(new(xorm.Session)) // handle fail and auto rollback
+}
--- go.mod
+++ go.mod
@@ -8,9 +8,11 @@ go 1.14
 //require golang.org/x/sys v0.0.0-20190329044733-9eb1bfa1ce65 // indirect
 
 require (
+	github.com/go-xorm/xorm v0.7.9
+	github.com/handsomestWei/go-annotation v0.0.0-20201013025858-335644dfb2d9
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1
 	github.com/satori/go.uuid v1.2.0
-	golang.org/x/text v0.3.0
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	rsc.io/qr v0.2.0
 )
