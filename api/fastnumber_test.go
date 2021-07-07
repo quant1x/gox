c7656f9b13e18d2b113c41179582cd8749981e7e
wangfeng <wangfengxy@sina.cn>
2021-07-07 10:38:32 +0800

    增加安全的字符串转数值的方法

---
+++ api/fastnumber_test.go
@@ -0,0 +1,123 @@
+package api
+
+import "testing"
+
+func TestParseFloat(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ParseFloat(tt.args.s); got != tt.want {
+				t.Errorf("ParseFloat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParseUint(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ParseUint(tt.args.s); got != tt.want {
+				t.Errorf("ParseUint() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParseint(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Parseint(tt.args.s); got != tt.want {
+				t.Errorf("Parseint() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseBestEffort(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseBestEffort(tt.args.s); got != tt.want {
+				t.Errorf("parseBestEffort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseInt64BestEffort(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseInt64BestEffort(tt.args.s); got != tt.want {
+				t.Errorf("parseInt64BestEffort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseUint64BestEffort(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want uint64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseUint64BestEffort(tt.args.s); got != tt.want {
+				t.Errorf("parseUint64BestEffort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
