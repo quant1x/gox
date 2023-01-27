ee4fa7dc608681d727e38824d596abe16f126a6b
wangfeng <wangfengxy@sina.cn>
2023-01-27 15:29:32 +0800

    增加一对字节数组和字符串互转的函数

---
+++ api/string_bytes.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+// Bytes2String 字节数组转字符串
+func Bytes2String(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// String2Bytes 字符串转字节数组
+func String2Bytes(s string) []byte {
+	strh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	var sh reflect.SliceHeader
+	sh.Data = strh.Data
+	sh.Len = strh.Len
+	sh.Cap = strh.Len
+	return *(*[]byte)(unsafe.Pointer(&sh))
+}
