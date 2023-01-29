79e84ab9429f80065860e17ed86ab0b057f2c153
wangfeng <wangfengxy@sina.cn>
2023-01-29 10:38:00 +0800

    增加推送所有tag的命令

--- push-gitee.sh
+++ push-gitee.sh
@@ -4,4 +4,5 @@ set -e
 
 git remote set-url origin https://gitee.com/mymmsc/gox.git
 git push --all
+git push --tags
 git remote -vv
--- push-github.sh
+++ push-github.sh
@@ -3,4 +3,5 @@
 set -e
 git remote set-url origin https://gitee.com/mymmsc/gox.git
 git push --all
+git push --tags
 git remote -vv
