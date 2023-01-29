e0ee3fb7adf866e5eaefd49bdc4aa3b717356842
wangfeng <wangfengxy@sina.cn>
2023-01-29 10:27:35 +0800

    增加gitee和github两个仓库的代码同步脚本

---
+++ push-gitee.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git remote set-url origin https://github.com/mymmsc/gox.git
+git push --all
---
+++ push-github.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git remote set-url origin https://gitee.com/mymmsc/gox.git
+git push --all
