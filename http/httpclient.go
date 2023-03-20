72701be5556969a7eb7cf4a001cc0bad388a0bd9
wangfeng <wangfengxy@sina.cn>
2023-03-20 12:07:58 +0800

    优化http client

--- http/httpclient.go
+++ http/httpclient.go
@@ -6,74 +6,93 @@ import (
 	"compress/gzip"
 	"github.com/mymmsc/gox/api"
 	"github.com/mymmsc/gox/logger"
-	"golang.org/x/text/encoding/simplifiedchinese"
-	"golang.org/x/text/transform"
 	"io"
 	"net/http"
 	URL "net/url"
 	"strings"
+	"time"
 )
 
-func HttpGet0(url string) ([]byte, error) {
-	logger.Debugf("url=[%s]\n", url)
-	res, err := http.Get(url)
-	if err != nil {
-		logger.Errorf("url=[%s], err=[%+v]\n", url, err)
-		return nil, err
-	}
-	defer api.CloseQuietly(res.Body)
-	data, err := io.ReadAll(transform.NewReader(res.Body, simplifiedchinese.GBK.NewDecoder()))
-	if err != nil {
-		logger.Errorf("url=[%s], err=[%+v]\n", url, err)
-		return nil, err
-	}
-	//sret, err := json.Marshal(res)
-	// request={}, params=[{}], http-status=[{}], body=[{}], message=[{}], acrossTime={}
-	status := res.StatusCode
-	response := data
-	logger.Infof("url=[%s], http-status=[%d], response=[%s], error=[%+v]\n", url, status, response, err)
-	return data, nil
+const (
+	ContentEncoding = "Content-Encoding"
+	LastModified    = "Last-Modified"
+	IfModifiedSince = "If-Modified-Since"
+)
+
+var (
+	TimeZero = time.Unix(0, 0)
+)
+
+type Response struct {
+	StatusCode    int
+	ContentLength int
+	LastModified  time.Time
+	Body          []byte
 }
 
 func HttpRequest(url string, method string) ([]byte, error) {
+	data, lastModified, err := Request(url, method)
+	_ = lastModified
+	return data, err
+}
+
+func HttpGet(url string) ([]byte, error) {
+	return HttpRequest(url, "get")
+}
+
+// Request http request, 支持传入header
+func Request(url string, method string, header ...map[string]any) (data []byte, lastModified time.Time, err error) {
 	u, err := URL.Parse(url)
 	if err != nil {
-		return nil, err
+		return nil, TimeZero, err
 	}
-	header := make(map[string]string)
-	header["Accept"] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
-	header["Accept-Encoding"] = "gzip, deflate"
-	header["Accept-Language"] = "zh-CN,zh;q=0.9,en;q=0.8"
-	header["Cache-Control"] = "no-cache"
-	header["Connection"] = "keep-alive"
-	header["Host"] = u.Host
-	header["Pragma"] = "no-cache"
-	header["Upgrade-Insecure-Requests"] = "1"
-	header["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+	reqHeader := make(map[string]string)
+	reqHeader["Accept"] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+	reqHeader["Accept-Encoding"] = "gzip, deflate"
+	reqHeader["Accept-Language"] = "zh-CN,zh;q=0.9,en;q=0.8"
+	reqHeader["Cache-Control"] = "no-cache"
+	reqHeader["Connection"] = "keep-alive"
+	reqHeader["Host"] = u.Host
+	reqHeader["Pragma"] = "no-cache"
+	reqHeader["Upgrade-Insecure-Requests"] = "1"
+	reqHeader["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+	if len(header) > 0 {
+		mapHeader := header[0]
+		for k, v := range mapHeader {
+			switch val := v.(type) {
+			case time.Time:
+				reqHeader[k] = val.Format(time.RFC1123)
+			}
+		}
+	}
 
 	client := &http.Client{}
 	request, err := http.NewRequest(strings.ToUpper(method), url, nil)
 	if err != nil {
-		return nil, err
+		return nil, TimeZero, err
 	}
-	for key, v := range header {
+	for key, v := range reqHeader {
 		request.Header.Add(key, v)
 	}
 
 	response, err := client.Do(request)
 	if err != nil {
-		return nil, err
+		return nil, TimeZero, err
 	}
+	lm := response.Header.Get(LastModified)
+	if response.StatusCode == http.StatusNotModified && !api.IsEmpty(lm) {
+		return nil, TimeZero, nil
+	}
+	lastModified, err = time.Parse(time.RFC1123, lm)
 	defer api.CloseQuietly(response.Body)
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return nil, err
+		return nil, TimeZero, err
 	}
-	contentEncoding := response.Header.Get("Content-Encoding")
+	contentEncoding := response.Header.Get(ContentEncoding)
 	var reader io.ReadCloser = nil
 	if len(contentEncoding) > 0 {
 		contentEncoding = strings.ToLower(contentEncoding)
-
 		switch contentEncoding {
 		case "gzip":
 			reader, err = gzip.NewReader(bytes.NewBuffer(body))
@@ -89,12 +108,8 @@ func HttpRequest(url string, method string) ([]byte, error) {
 		defer api.CloseQuietly(reader)
 		body, err = io.ReadAll(reader)
 		if err != nil {
-			return nil, err
+			return nil, TimeZero, err
 		}
 	}
-	return body, nil
-}
-
-func HttpGet(url string) ([]byte, error) {
-	return HttpRequest(url, "get")
+	return body, lastModified, nil
 }
