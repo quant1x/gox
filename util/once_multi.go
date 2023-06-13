9ed02638695fbc79aedbcb6aba02a9c11ee05d58
wangfeng <wangfengxy@sina.cn>
2023-06-14 07:37:52 +0800

    新增MultiOnce组件, 可以重置的Sync.Once

---
+++ util/once_multi.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+type MultiOnce struct {
+	done uint32
+	m    sync.Mutex
+}
+
+func (o *MultiOnce) Do(f func()) {
+	if atomic.LoadUint32(&o.done) == 0 {
+		o.doSlow(f)
+	}
+}
+
+func (o *MultiOnce) doSlow(f func()) {
+	o.m.Lock()
+	defer o.m.Unlock()
+	if o.done == 0 {
+		defer atomic.StoreUint32(&o.done, 1)
+		f()
+	}
+}
+
+func (o *MultiOnce) Reset() {
+	atomic.StoreUint32(&o.done, 0)
+}
