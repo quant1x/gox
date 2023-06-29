e96db364d3322db8514cb5739dcc0535686f0a17
wangfeng <wangfengxy@sina.cn>
2023-06-30 05:56:39 +0800

    增加日期切换功能, 默认不开启

--- util/once_multi.go
+++ util/once_multi.go
@@ -8,11 +8,33 @@ import (
 type MultiOnce struct {
 	done uint32
 	m    sync.Mutex
+	date string
 }
 
-func (o *MultiOnce) Do(f func()) {
+func (o *MultiOnce) Do(f func(), today ...func() (newDate string)) {
+	var getToday func() (newDate string)
+	if len(today) > 0 {
+		getToday = today[0]
+	}
+	if getToday != nil {
+		currentDate := getToday()
+		if atomic.LoadUint32(&o.done) == 1 && currentDate > o.date {
+			o.doReset(currentDate)
+		}
+	}
 	if atomic.LoadUint32(&o.done) == 0 {
 		o.doSlow(f)
+	}
+}
+
+func (o *MultiOnce) doReset(currentDate string) {
+	o.m.Lock()
+	defer o.m.Unlock()
+	if o.done == 1 && currentDate > o.date {
+		o.m.Lock()
+		defer o.m.Unlock()
+		atomic.StoreUint32(&o.done, 0)
+		o.date = currentDate
 	}
 }
 
@@ -27,4 +49,8 @@ func (o *MultiOnce) doSlow(f func()) {
 
 func (o *MultiOnce) Reset() {
 	atomic.StoreUint32(&o.done, 0)
+}
+
+func (o *MultiOnce) Date() string {
+	return o.date
 }
