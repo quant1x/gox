59ae60def3ba682306607be786f84788269fe850
wangfeng <wangfengxy@sina.cn>
2023-07-07 11:07:53 +0800

    判断float是否NaN

--- num/decimals.go
+++ num/decimals.go
@@ -2,11 +2,18 @@ package num
 
 import "math"
 
+func IsNaN(f float64) bool {
+	return math.IsNaN(f) || math.IsInf(f, 0)
+}
+
 // Decimal 保留小数点四舍五入
 func Decimal(value float64, digits ...int) float64 {
 	defaultDigits := 2
 	if len(digits) > 0 {
 		defaultDigits = digits[0]
+	}
+	if IsNaN(value) {
+		value = float64(0)
 	}
 	n10 := math.Pow10(defaultDigits)
 	return math.Trunc((value+0.5/n10)*n10) / n10
