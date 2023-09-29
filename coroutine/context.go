d11d311b17816e2813b6654943420ed0d4082317
wangfeng <wangfengxy@sina.cn>
2023-09-29 10:35:26 +0800

    增加context.Context的封装

--- coroutine/context.go
+++ coroutine/context.go
@@ -1,1 +1,36 @@
 package coroutine
+
+import (
+	"context"
+	"sync"
+)
+
+var (
+	globalOnce    sync.Once
+	globalContext context.Context    = nil
+	globalCancel  context.CancelFunc = nil
+)
+
+func initContext() {
+	globalContext, globalCancel = context.WithCancel(context.Background())
+}
+
+// Context 获取全局顶层context
+func Context() context.Context {
+	globalOnce.Do(initContext)
+	return globalContext
+}
+
+// Shutdown 关闭应用程序, 通知所有协程退出
+func Shutdown() {
+	globalOnce.Do(initContext)
+	if globalCancel != nil {
+		globalCancel()
+	}
+}
+
+func GetContextWithCancel() (context.Context, context.CancelFunc) {
+	globalOnce.Do(initContext)
+	ctx, cancel := context.WithCancel(globalContext)
+	return ctx, cancel
+}
