b80d41595cc4f88aeccd5e406df30ee7e07ff213
wangfeng <wangfengxy@sina.cn>
2023-10-01 13:47:22 +0800

    优化fastqueue的push为异步方式

--- fastqueue/fastqueue.go
+++ fastqueue/fastqueue.go
@@ -3,6 +3,7 @@ package fastqueue
 import (
 	"errors"
 	"math/bits"
+	"sync"
 )
 
 const (
@@ -22,7 +23,8 @@ type Queue[T any] struct {
 	bufferSize int64
 	indexMask  int64
 	onData     func([]T)
-	done       chan bool
+	waitGroup  sync.WaitGroup
+	//done       chan bool
 }
 
 //// math/bits.OnesCount64
@@ -37,11 +39,37 @@ type Queue[T any] struct {
 //	return int(x)
 //}
 
+func highestOneBit(x uint64) uint64 {
+	origin := x
+	isPower2 := ((origin & (origin - 1)) == 0) && (origin != 0)
+	if isPower2 {
+		return origin
+	}
+	// HD, Figure 3-1
+	x |= x >> 1
+	x |= x >> 2
+	x |= x >> 4
+	x |= x >> 8
+	x |= x >> 16
+	x |= x >> 32
+	//x |= (x >> 64)
+	//x = x - (x >> 1)
+	x = x & ^(x >> 1)
+	if x < origin {
+		x <<= 1
+	}
+	if x == 0 {
+		x = 1 << 63
+	}
+	return x
+}
+
 func NewQueue[T any](max ...int) *Queue[T] {
 	buffSize := int64(kBufferSize)
 	if len(max) > 0 {
 		buffSize = int64(max[0])
 	}
+	buffSize = int64(highestOneBit(uint64(buffSize)))
 	if bits.OnesCount64(uint64(buffSize)) != 1 {
 		panic(errBufferSizeNotPower2)
 	}
@@ -57,11 +85,19 @@ func NewQueue[T any](max ...int) *Queue[T] {
 	return queue
 }
 
+func (q *Queue[T]) Finish() {
+	_ = q.disruptor.Close()
+}
+
 func (q *Queue[T]) Wait() {
+	go func() {
+		q.waitGroup.Wait()
+		q.Finish()
+	}()
 	q.disruptor.Read()
 }
 
-func (q *Queue[T]) SetEvent(event func(data []T)) {
+func (q *Queue[T]) SetReadEvent(event func(data []T)) {
 	q.onData = event
 }
 
@@ -76,7 +112,17 @@ func (q *Queue[T]) Consume(lower, upper int64) {
 	}
 }
 
-func (q *Queue[T]) pushOne(data T) {
+func queuePushMessage[T any](queue *Queue[T], data T) {
+	defer queue.waitGroup.Done()
+	kReservations := int64(1)
+	sequence := queue.disruptor.Reserve(kReservations)
+	for lower := sequence - kReservations + 1; lower <= sequence; lower++ {
+		queue.ringBuffer[lower&queue.indexMask] = data
+	}
+	queue.disruptor.Commit(sequence-kReservations+1, sequence)
+}
+
+func (q *Queue[T]) pushOne_old(data T) {
 	kReservations := int64(1)
 	sequence := q.disruptor.Reserve(kReservations)
 	for lower := sequence - kReservations + 1; lower <= sequence; lower++ {
@@ -85,12 +131,17 @@ func (q *Queue[T]) pushOne(data T) {
 	q.disruptor.Commit(sequence-kReservations+1, sequence)
 }
 
+func (q *Queue[T]) PushOne(data T) {
+	q.waitGroup.Add(1)
+	go queuePushMessage(q, data)
+}
+
 func (q *Queue[T]) Push(data ...T) {
 	num := len(data)
 	if num == 0 {
 		return
 	}
 	for _, v := range data {
-		q.pushOne(v)
+		q.PushOne(v)
 	}
 }
--- fastqueue/fastqueue_test.go
+++ fastqueue/fastqueue_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"math/bits"
 	"testing"
-	"time"
 )
 
 const (
@@ -13,25 +12,54 @@ const (
 )
 
 func TestQueue_Consume(t *testing.T) {
-	queue := NewQueue[string](tIterations)
-	queue.SetEvent(func(data []string) {
+	queue := NewQueue[string](tIterations - 1)
+	queue.SetReadEvent(func(data []string) {
 		for _, v := range data {
+			//time.Sleep(time.Second * 1)
 			fmt.Println(v)
 		}
 	})
-	//wg := sync.WaitGroup{}
-	for i := 0; i < tIterations; i++ {
-		//wg.Add(1)
+	for i := 0; i < tIterations*2; i++ {
 		message := fmt.Sprintf("%d", i)
-		queue.pushOne(message)
+		go queue.Push(message)
 	}
-	//wg.Wait()
+
+	//time.Sleep(time.Second * 10)
+	//go queue.PushOne("128")
+	//queue.Finish()
 	queue.Wait()
-	time.Sleep(time.Second * 10000)
+	//time.Sleep(time.Second * 1)
+}
+
+// #define APR_ALIGN(size, boundary)    (((size) + ((boundary) - 1)) & ~((boundary) - 1))
+//
+//	将size往上"取整到"大于size的最小的boundary的倍数
+func AlignUint(x uint64) uint64 {
+	const boundary uint64 = 2
+	y := ((x) + ((boundary) - 1)) & ^((boundary) - 1)
+	return y
 }
 
 func Test_bitCount(t *testing.T) {
-	fmt.Println(bits.OnesCount64(127))
+	fmt.Println(highestOneBit(1))
+	fmt.Println(highestOneBit(2))
+	fmt.Println(highestOneBit(3))
+	fmt.Println(highestOneBit(4))
+	fmt.Println(highestOneBit(126))
+	fmt.Println(highestOneBit(127))
+	fmt.Println(highestOneBit(128))
+	fmt.Println(highestOneBit(0xffffffffffffffff))
+	var x uint64 = 3
+	xl := bits.Len64(x)
+	if xl%2 != 0 {
+		xl++
+	}
+	x = 1 << (xl)
+	fmt.Println(bits.OnesCount64(x))
 	fmt.Println(bits.OnesCount64(128))
 	fmt.Println(bits.OnesCount64(256))
+
+	fmt.Println(x, "len =", bits.Len64(x))
+	x = 128
+	fmt.Println(x, "len =", bits.Len64(x))
 }
