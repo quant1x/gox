9002ed727d48199c38e36ab97f712ead0cbcbbf2
wangfeng <wangfengxy@sina.cn>
2023-10-20 11:01:08 +0800

    调整任务未执行完成跳过的函数封装

--- cron/cron.go
+++ cron/cron.go
@@ -97,17 +97,17 @@ func (s byTime) Less(i, j int) bool {
 //
 // Available Settings
 //
-//   Time Zone
-//     Description: The time zone in which schedules are interpreted
-//     Default:     time.Local
+//	Time Zone
+//	  Description: The time zone in which schedules are interpreted
+//	  Default:     time.Local
 //
-//   Parser
-//     Description: Parser converts cron spec strings into cron.Schedules.
-//     Default:     Accepts this spec: https://en.wikipedia.org/wiki/Cron
+//	Parser
+//	  Description: Parser converts cron spec strings into cron.Schedules.
+//	  Default:     Accepts this spec: https://en.wikipedia.org/wiki/Cron
 //
-//   Chain
-//     Description: Wrap submitted jobs to customize behavior.
-//     Default:     A chain that recovers panics and logs them to stderr.
+//	Chain
+//	  Description: Wrap submitted jobs to customize behavior.
+//	  Default:     A chain that recovers panics and logs them to stderr.
 //
 // See "cron.With*" to modify the default behavior.
 func New(opts ...Option) *Cron {
@@ -151,6 +151,11 @@ func (c *Cron) AddJob(spec string, cmd Job) (EntryID, error) {
 		return 0, err
 	}
 	return c.Schedule(schedule, cmd), nil
+}
+
+func (c *Cron) AddFuncWithSkipIfStillRunning(spec string, cmd func()) (EntryID, error) {
+	job := SkipIfStillRunning()(FuncJob(cmd))
+	return c.AddJob(spec, job)
 }
 
 // Schedule adds a Job to the Cron to be run on the given schedule.
--- cron/scheduler_test.go
+++ cron/scheduler_test.go
@@ -10,10 +10,10 @@ func TestScheduler(t *testing.T) {
 	c := New()
 	//c := New(WithSeconds())
 	c.Start()
-	_, err := c.AddJob("@every 1s", SkipIfStillRunning()(FuncJob(func() {
+	_, err := c.AddFuncWithSkipIfStillRunning("@every 1s", func() {
 		time.Sleep(time.Second * 3)
 		fmt.Printf("SkipIfStillRunningWithLogger: %v\n", time.Now())
-	})))
+	})
 	if err != nil {
 		fmt.Println(err)
 		return
