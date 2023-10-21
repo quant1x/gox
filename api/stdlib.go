de47f988a8cca3f51741977409771a120b0e6e7d
wangfeng <wangfengxy@sina.cn>
2023-10-22 06:42:20 +0800

    增加获取当前代码的函数名, 文件名以及行号的函数

---
+++ api/stdlib.go
@@ -0,0 +1,14 @@
+package api
+
+import "runtime"
+
+// Caller 获取正在运行的函数名, 文件名以及行号
+func Caller() (function, file string, line int) {
+	pcs := make([]uintptr, 1)
+	runtime.Callers(2, pcs)
+	pc := pcs[0]
+	f := runtime.FuncForPC(pc)
+	function = f.Name()
+	file, line = f.FileLine(pc)
+	return
+}
---
+++ api/stdlib_test.go
@@ -0,0 +1,10 @@
+package api
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFunction(t *testing.T) {
+	fmt.Println(Caller())
+}
