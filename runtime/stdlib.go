c97408a87bececdd2a20ac291dc021f751177d8a
wangfeng <wangfengxy@sina.cn>
2023-10-22 08:48:21 +0800

    增加获取func信息的函数

--- runtime/stdlib.go
+++ runtime/stdlib.go
@@ -1,14 +1,69 @@
 package runtime
 
-import "runtime"
+import (
+	"reflect"
+	"runtime"
+	_ "unsafe"
+)
+
+const (
+	invalidProgramCounters = 0
+	invalidFunctionName    = "<invalid runtimeFunctionForPC>"
+	unknownFilename        = "<unknown file>"
+	unknownLine            = -1
+)
 
 // Caller 获取正在运行的函数名, 文件名以及行号
 func Caller() (function, file string, line int) {
 	pcs := make([]uintptr, 1)
 	runtime.Callers(2, pcs)
 	pc := pcs[0]
-	f := runtime.FuncForPC(pc)
-	function = f.Name()
-	file, line = f.FileLine(pc)
+	return FuncInfo(pc)
+}
+
+//go:linkname runtimeFunctionForPC runtime.FuncForPC
+func runtimeFunctionForPC(pc uintptr) *runtime.Func
+
+// 获取pc
+func programCounters(v any) uintptr {
+	value := reflect.ValueOf(v)
+	var pc uintptr
+	if value.Kind() == reflect.Func {
+		pc = value.Pointer()
+	} else if value.Kind() == reflect.Uintptr {
+		pc = v.(uintptr)
+	} else {
+		pc = invalidProgramCounters
+	}
+	return pc
+}
+
+// FuncInfo 获取函数信息
+//
+//	v 接收func类型和uintptr
+func FuncInfo(v any) (nameOfFunction, filename string, lineNumber int) {
+	pc := programCounters(v)
+	if pc == invalidProgramCounters {
+		return invalidFunctionName, unknownFilename, unknownLine
+	}
+	f := runtimeFunctionForPC(pc)
+	if f == nil {
+		return invalidFunctionName, unknownFilename, unknownLine
+	}
+	nameOfFunction = f.Name()
+	filename, lineNumber = f.FileLine(pc)
 	return
+}
+
+// FuncName 获取函数名
+func FuncName(v any) (nameOfFunction string) {
+	pc := programCounters(v)
+	if pc == invalidProgramCounters {
+		return invalidFunctionName
+	}
+	f := runtimeFunctionForPC(pc)
+	if f == nil {
+		return invalidFunctionName
+	}
+	return f.Name()
 }
--- runtime/stdlib_test.go
+++ runtime/stdlib_test.go
@@ -2,9 +2,26 @@ package runtime
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
+func test1(cb func()) {
+	v := reflect.ValueOf(cb)
+	fmt.Println(v)
+	//p := unsafe.Pointer(&cb)
+	//pc := uintptr(p)
+	pc := v.Pointer()
+	funcName := FuncName(pc)
+	fmt.Println(funcName)
+}
+
+func foo() {
+	fmt.Println(1)
+}
+
 func TestFunction(t *testing.T) {
 	fmt.Println(Caller())
+	test1(foo)
+	fmt.Println(FuncInfo(foo))
 }
