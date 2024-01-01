c6aa0bea84c134f0cd5c418919f4dd57f91d1d15
wangfeng <wangfengxy@sina.cn>
2024-01-01 08:03:29 +0800

    增加功能性函数, 捕获panic, 忽略异常, 继续执行

--- runtime/recover.go
+++ runtime/recover.go
@@ -14,3 +14,11 @@ func CatchPanic() {
 		logger.Fatalf("%s 异常: %+v", ApplicationName(), err)
 	}
 }
+
+// IgnorePanic 通用捕获panic, 忽略异常, 继续执行
+func IgnorePanic() {
+	if err := recover(); err != nil {
+		s := string(debug.Stack())
+		logger.Errorf("err=%v, stack=%s", err, s)
+	}
+}
