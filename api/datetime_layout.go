2d7087cc34a00c3540976de695458c4b9c433dd9
wangfeng <wangfengxy@sina.cn>
2024-01-08 07:38:49 +0800

    调整部分代码

--- api/datetime_layout.go
+++ api/datetime_layout.go
@@ -1,5 +1,7 @@
 package api
 
+import "time"
+
 const (
 	// DateFormat  日期格式 yyyy-MM-dd
 	DateFormat = "2006-01-02"
@@ -12,9 +14,13 @@ const (
 	TimeFormat = "2006-01-02 15:04:05"
 	// TimeFormat2 时间格式 yyyyMMddHHmmss
 	TimeFormat2 = "20060102150405"
-
 	// Timestamp 时间戳 - 毫秒 时间格式 yyyy-MM-dd HH:mm:ss.SSS
 	Timestamp = "2006-01-02 15:04:05.000"
+
+	// TimeOnly 时分秒
+	TimeOnly = time.TimeOnly
+	// TimeAndMillisecond 时分秒毫
+	TimeAndMillisecond = "15:04:05.000"
 
 	PathDate = "20060102"   // 路径中的日期格式
 	TextDate = "2006-01-02" // 数据中的日期格式
