bfcf142a21cc1ddadc02383fe95bb3731d29efdc
wangfeng <wangfengxy@sina.cn>
2024-01-23 14:50:02 +0800

    修订日志使用应用程序名时去掉扩展名

--- logger/application.go
+++ logger/application.go
@@ -3,22 +3,24 @@ package logger
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"sync"
 )
 
 var (
-	onceApp     sync.Once
-	application = ""
+	onceApp         sync.Once
+	applicationName = ""
 )
 
 func lazyLoadApplication() {
 	path, _ := os.Executable()
 	_, exec := filepath.Split(path)
-	application = exec
+	arr := strings.Split(exec, ".")
+	applicationName = arr[0]
 }
 
 // ApplicationName 获取执行文件名
 func ApplicationName() string {
 	onceApp.Do(lazyLoadApplication)
-	return application
+	return applicationName
 }
