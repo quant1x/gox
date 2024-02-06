c13db55ec929b934b59222abb70aa95e8eb536fe
wangfeng <wangfengxy@sina.cn>
2024-02-06 09:33:57 +0800

    新增mmap转slice功能

--- cache/fast.go
+++ cache/fast.go
@@ -1,49 +1,80 @@
 package cache
 
-//import (
-//	"os"
-//	"path/filepath"
-//	"syscall"
-//)
-//
-//type FastCache struct {
-//	filename string
-//	f        *os.File
-//	size     int64
-//	Data     []byte
-//}
-//
-//func OpenCache(name string, size int64) (*FastCache, error) {
-//	dir := filepath.Dir(name)
-//	err := os.MkdirAll(dir, 0755)
-//	if err != nil {
-//		return nil, err
-//	}
-//	filename := name
-//	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
-//	if nil != err {
-//		return nil, err
-//	}
-//	err = f.Truncate(size)
-//	if nil != err {
-//		return nil, err
-//	}
-//	data, err := syscall.Mmap(int(f.Fd()), 0, int(size), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
-//	if nil != err {
-//		return nil, err
-//	}
-//
-//	return &FastCache{
-//		filename: filename,
-//		f:        f,
-//		size:     size,
-//		Data:     data,
-//	}, nil
-//}
-//
-//func (fc *FastCache) Close() {
-//	if fc != nil && fc.f != nil {
-//		_ = fc.f.Close()
-//		_ = syscall.Munmap(fc.Data)
-//	}
-//}
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"syscall"
+	"unsafe"
+)
+
+type FastCache struct {
+	filename string
+	f        *os.File
+	size     int64
+	//Data     []byte
+	Data MMap
+}
+
+func OpenCache(name string, size int64) (*FastCache, error) {
+	dir := filepath.Dir(name)
+	err := os.MkdirAll(dir, 0755)
+	if err != nil {
+		return nil, err
+	}
+	filename := name
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	if nil != err {
+		return nil, err
+	}
+	err = f.Truncate(size)
+	if nil != err {
+		return nil, err
+	}
+	//data, err := syscall.Mmap(int(f.Fd()), 0, int(size), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
+	data, err := mmap(int(size), RDWR, 0, f.Fd(), 0)
+	if nil != err {
+		return nil, err
+	}
+
+	return &FastCache{
+		filename: filename,
+		f:        f,
+		size:     size,
+		Data:     data,
+	}, nil
+}
+
+func (fc *FastCache) Close() {
+	if fc == nil {
+		return
+	}
+	err := fc.Data.Flush()
+	_ = syscall.Munmap(fc.Data)
+	if fc.f != nil {
+		_ = fc.f.Close()
+	}
+	_ = err
+}
+
+func ToSlices2[E any](fc *FastCache) []E {
+	ptr := &fc.Data[0]
+	fmt.Printf("fc.Data addr=%p\n", fc.Data)
+	//unsafe.Slice(ptr, 2)
+	p := (*[1 << 32]E)(unsafe.Pointer(ptr))[:]
+	fmt.Printf("p addr=%p\n", p)
+	return p
+}
+
+func ToSlices[E any](fc *FastCache) []E {
+	var tmpValue E
+	size := int(unsafe.Sizeof(tmpValue))
+	num := int(fc.size) / size
+	ptr := &fc.Data[0]
+	//fmt.Printf("fc.Data addr=%p\n", fc.Data)
+	//unsafe.Slice(ptr, 2)
+	p := (*[1 << 32]E)(unsafe.Pointer(ptr))[:]
+	//fmt.Printf("p addr=%p\n", p)
+	//p = slices.Grow(p, num)
+	return p[:0:num]
+}
