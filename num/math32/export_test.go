2e4abc194c73fcf3bd44c07c81ecdfcff9fed368
wangfeng <wangfengxy@sina.cn>
2024-02-12 09:31:24 +0800

    独立拆分num工具包

--- num/math32/export_test.go
+++
@@ -1,8 +0,0 @@
-package math32
-
-// Export internal functions for testing.
-
-// var Exp2Go = exp2
-var SqrtGo = sqrt
-var ExpGo = exp
-var HypotGo = hypot
--- num/math32/round.go
+++
@@ -1,7 +0,0 @@
-package math32
-
-import "math"
-
-func Round(value float32) float32 {
-	return float32(math.Round(float64(value)))
-}
