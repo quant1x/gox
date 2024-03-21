49bd781ce540a7628fe3efba4b3e79957f909b66
wangfeng <wangfengxy@sina.cn>
2024-03-21 11:29:30 +0800

    切片为空就是清空文件

--- api/slices_csv.go
+++ api/slices_csv.go
@@ -31,9 +31,6 @@ func CsvToSlices[S ~[]E, E any](filename string, pointer *S) error {
 
 // SlicesToCsv struct切片转csv文件
 func SlicesToCsv[S ~[]E, E any](filename string, s S) error {
-	if len(s) == 0 {
-		return nil
-	}
 	filepath, err := homedir.Expand(filename)
 	if err != nil {
 		return err
