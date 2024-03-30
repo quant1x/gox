44424b259473e95792fb4f6c32b0e9cc3f747296
wangfeng <wangfengxy@sina.cn>
2024-03-30 10:45:30 +0800

    修订同步github代码的repo

--- push-github.sh
+++ push-github.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-git remote set-url origin https://github.com/mymmsc/gox.git
+git remote set-url origin https://github.com/quant1x/gox.git
 git push --all
 git push --tags
 git remote -vv
