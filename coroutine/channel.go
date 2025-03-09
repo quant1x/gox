a911636bf83f4b679aced9e4526e579451cb0500
wangfeng <wangfengxy@sina.cn>
2025-03-10 06:20:44 +0800

    优化泛型Channel的数据功能

--- coroutine/channel.go
+++ coroutine/channel.go
@@ -1,39 +1,78 @@
 package coroutine
 
 import (
+	"errors"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
-// Channel 多协程安全的channel
+var (
+	ErrChannelClosed = errors.New("channel closed")
+	ErrBufferFull    = errors.New("buffer full")
+)
+
 type Channel[E any] struct {
-	data chan E
-	m    sync.Mutex
-	num  atomic.Int64
-	done atomic.Uint32
+	data      chan E
+	closeOnce sync.Once
+	closed    atomic.Bool
+	num       atomic.Int64
 }
 
-func (c *Channel[E]) Push(v E) {
-	if c.done.Load() == 1 {
-		return
+func NewChannel[E any](bufSize int) *Channel[E] {
+	return &Channel[E]{
+		data: make(chan E, bufSize),
 	}
-	c.data <- v
-	c.num.Add(1)
 }
 
-func (c *Channel[E]) Pop() E {
-	v := <-c.data
-	c.num.Add(-1)
-	return v
+func (c *Channel[E]) Push(v E) error {
+	if c.closed.Load() {
+		return ErrChannelClosed
+	}
+
+	select {
+	case c.data <- v:
+		c.num.Add(1)
+		return nil
+	default:
+		return ErrBufferFull
+	}
+}
+
+// SafePush 带超时的安全推送
+func (c *Channel[E]) SafePush(v E, timeout time.Duration) error {
+	if c.closed.Load() {
+		return ErrChannelClosed
+	}
+
+	select {
+	case c.data <- v:
+		c.num.Add(1)
+		return nil
+	case <-time.After(timeout):
+		return ErrBufferFull
+	}
+}
+
+func (c *Channel[E]) Pop() (E, bool) {
+	v, ok := <-c.data
+	if ok {
+		c.num.Add(-1)
+	}
+	return v, ok
 }
 
 func (c *Channel[E]) Close() {
-	if c.done.Load() != 0 {
-		return
-	}
-	c.m.Lock()
-	defer c.m.Unlock()
-	if c.done.Load() == 0 {
-		c.done.Store(1)
-	}
+	c.closeOnce.Do(func() {
+		close(c.data)
+		c.closed.Store(true)
+	})
+}
+
+func (c *Channel[E]) Len() int {
+	return int(c.num.Load())
+}
+
+func (c *Channel[E]) Cap() int {
+	return cap(c.data)
 }
