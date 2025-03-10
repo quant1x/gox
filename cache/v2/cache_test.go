a1556b1a05d7aa55d314b6f23342c5901ebdf135
wangfeng <wangfengxy@sina.cn>
2025-03-10 15:55:22 +0800

    新增第2个版本的cache功能

---
+++ cache/v2/cache_test.go
@@ -0,0 +1,45 @@
+package cache
+
+import (
+	"fmt"
+	"gitee.com/quant1x/gox/api"
+	"testing"
+	"unsafe"
+)
+
+const (
+	filename = "data.dat"
+)
+
+type TT struct {
+	A int
+	B [6]byte
+}
+
+func TestToSlice(t *testing.T) {
+	num := 2
+	size := int64(unsafe.Sizeof(TT{}))
+
+	fc, err := OpenCache(filename, size*int64(num))
+	if err != nil {
+		fmt.Println(fc, err)
+		return
+	}
+	defer fc.Close()
+	list, err := ToSlice[TT](fc)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for i := 0; i < num; i++ {
+		d := i + 10
+		var tt TT
+		tt.A = d
+		str := fmt.Sprintf("%d", d)
+		b := [6]byte{}
+		copy(b[:], api.String2Bytes(str))
+		copy(tt.B[:], b[:])
+		list[i] = tt
+	}
+	fmt.Println(len(list))
+	fmt.Printf("%+v\n", list)
+}
