2303bc4a88ac4ae173878639a7b77407763752ea
wangfeng <wangfengxy@sina.cn>
2025-09-14 21:25:32 +0800

    删除废弃的gitee和github同步脚本

--- push-gitee.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-git remote set-url origin https://gitee.com/quant1x/gox.git
-git checkout master
-git fetch --all
-git pull --all
-git remote -vv
--- push-github.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-git remote set-url origin https://github.com/quant1x/gox.git
-git push --all
-git push --tags
-git remote -vv
